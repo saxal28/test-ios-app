86c809448485152fbb61c528d3acd9567ce839c1
Alan <saxal28@gmail.com>
2018-03-23 17:24:08 -0500

    test

---
+++ Alan's Monster Test App/Controllers/LiveTextVC.swift
@@ -0,0 +1,29 @@
+//
+//  LiveTextVC.swift
+//  Alan's Monster Test App
+//
+//  Created by alan on 3/22/18.
+//  Copyright © 2018 alan. All rights reserved.
+//
+
+import UIKit
+
+class LiveTextVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    //=====================================================
+    //MARK: IBOutlets
+    //=====================================================
+    
+    
+    
+    //=====================================================
+    //MARK: IBOutlets
+    //=====================================================
+
+    
+}
